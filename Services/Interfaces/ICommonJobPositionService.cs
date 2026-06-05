06a32de6275193c42978acd151713d85dc939b61
Kien Hoang <hoangminhkien6666@gmail.com>
2026-06-05 08:03:42 +0700

    fix job

--- Services/Interfaces/ICommonJobPositionService.cs
+++ Services/Interfaces/ICommonJobPositionService.cs
@@ -1,5 +1,10 @@
-﻿namespace DevHub.Services.Interfaces;
+namespace DevHub.Services.Interfaces;
 
 public interface ICommonJobPositionService
 {
+    Task<List<DevHub.Models.CommonJobPosition>> GetAllPositionsAsync();
+    Task<DevHub.Models.CommonJobPosition?> GetPositionByIdAsync(int id);
+    Task UpdatePositionAsync(DevHub.Models.CommonJobPosition position);
+    Task AddPositionAsync(DevHub.Models.CommonJobPosition position);
+    Task<bool> ToggleStatusAsync(int id);
 }

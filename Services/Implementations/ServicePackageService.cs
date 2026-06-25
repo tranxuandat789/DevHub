18cbf2d2b23effb44d06a6036df456b71afcddb2
Kien Hoang <hoangminhkien6666@gmail.com>
2026-06-25 20:14:40 +0700

    Manage package service

--- Services/Implementations/ServicePackageService.cs
+++ Services/Implementations/ServicePackageService.cs
@@ -1,7 +1,42 @@
-﻿using DevHub.Services.Interfaces;
+using DevHub.Models;
+using DevHub.Repositories.Interfaces;
+using DevHub.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DevHub.Services.Implementations;
 
 public class ServicePackageService : IServicePackageService
 {
+    private readonly IServicePackageRepository _repository;
+
+    public ServicePackageService(IServicePackageRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Task<ServicePackage?> GetByIdAsync(int serviceId)
+    {
+        return _repository.GetByIdAsync(serviceId);
+    }
+
+    public Task<(List<ServicePackage> Items, int TotalCount)> GetAllPackagesAsync(string searchTerm, string statusFilter, string sortOrder, int page, int pageSize)
+    {
+        return _repository.GetAllPackagesAsync(searchTerm, statusFilter, sortOrder, page, pageSize);
+    }
+
+    public Task<ServicePackage> CreatePackageAsync(ServicePackage package)
+    {
+        return _repository.CreatePackageAsync(package);
+    }
+
+    public Task<ServicePackage> UpdatePackageAsync(ServicePackage package)
+    {
+        return _repository.UpdatePackageAsync(package);
+    }
+
+    public Task<bool> ToggleStatusAsync(int id, bool activate)
+    {
+        return _repository.ToggleStatusAsync(id, activate);
+    }
 }
--- Services/Interfaces/IServicePackageService.cs
+++ Services/Interfaces/IServicePackageService.cs
@@ -1,5 +1,14 @@
-﻿namespace DevHub.Services.Interfaces;
+using DevHub.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevHub.Services.Interfaces;
 
 public interface IServicePackageService
 {
+    Task<ServicePackage?> GetByIdAsync(int serviceId);
+    Task<(List<ServicePackage> Items, int TotalCount)> GetAllPackagesAsync(string searchTerm, string statusFilter, string sortOrder, int page, int pageSize);
+    Task<ServicePackage> CreatePackageAsync(ServicePackage package);
+    Task<ServicePackage> UpdatePackageAsync(ServicePackage package);
+    Task<bool> ToggleStatusAsync(int id, bool activate);
 }
